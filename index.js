8a05b27827331e5f5df4f9d66378780cfed6a3b5
sarthak bansal <39374452+sarthakworks@users.noreply.github.com>
2025-03-24 15:28:49 +0530

    initial

---
+++ index.js
@@ -0,0 +1,9 @@
+// Get the element with the id 'root'
+const rootElement = document.getElementById('root');
+
+// Create a new text node
+const textNode = document.createElement('h1');
+textNode.textContent = 'Hello, World!';
+
+// Append the text node to the #root element
+rootElement.appendChild(textNode);
